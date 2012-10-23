05567ba4b9a579ec00a4b9ab347f9c688f74a7f1
J. Ian Johnson <ianj@ccs.neu.edu>
2012-10-23 09:22:44 -0400

    Silly bug in restrict-to-reachable.

--- code/env.rkt
+++ code/env.rkt
@@ -5,22 +5,13 @@
 (define (extend ρ x v)
   (hash-set ρ x v))
 (define (extend* ρ xs vs)
-  (cond [(empty? xs) ρ]
-        [else (extend* (extend ρ (first xs) (first vs))
-                       (rest xs)
-                       (rest vs))]))
+  (for/fold ([ρ ρ]) ([x (in-list xs)] [v (in-list vs)])
+    (hash-set ρ x v)))
 (define (join σ a s)
   (hash-set σ a
             (∪ s (hash-ref σ a ∅))))
 (define (join* σ as ss)
   (for/fold ([σ σ]) ([a as] [s ss]) (join σ a s)))
-(define (join-one σ a x)
-  (hash-set σ a (∪1 (hash-ref σ a ∅) x)))
-(define (join-one* σ as xs)
-  (cond [(empty? as) σ]
-        [else (join-one* (join-one σ (first as) (first xs))
-                         (rest as)
-                         (rest xs))]))
 
 ;; Store Store -> Store
 (define (join-store σ1 σ2)
@@ -33,12 +24,6 @@
 (define (update ∆s σ)
   (for/fold ([σ σ]) ([a×vs (in-list ∆s)])
     (join σ (car a×vs) (cdr a×vs))))
-#;
-;; Set State -> Store
-(define (join-stores ss)
-  (for/fold ([σ (hash)])
-    ([s ss])
-    (join-store σ (state-σ s))))
 
 (define (((mk-reach ref) touches) σ root)
   (define seen ∅)
@@ -54,7 +39,7 @@
   (define reach ((mk-reach ref) touches))
   (λ (σ v)
      (for/hash ([a (in-set (reach σ (touches v)))])
-       (values a (hash-ref σ a)))))
+       (values a (ref σ a)))))
 
 (define reach (mk-reach hash-ref))
 (define reach/vec (mk-reach vector-ref))
