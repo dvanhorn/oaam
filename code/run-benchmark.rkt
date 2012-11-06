4999427eb0008890021df03d6810ea3736c9cdea
J. Ian Johnson <ianj@ccs.neu.edu>
2012-11-06 15:51:54 -0500

    Added greppable tag to run-benchmark result.

--- code/run-benchmark.rkt
+++ code/run-benchmark.rkt
@@ -15,12 +15,10 @@
 
 (define (print-values . vs) (for ([v vs]) (display v) (newline)))
 
-
 (define aval (make-parameter #f))
 
 (define (test e)
   (parameterize ([current-logger (make-logger 'stuck-states)])
-#;#;
     (log-thread 'info)
     (log-thread 'debug)
     ;; we want to make sure that we are testing the implementation and not
@@ -31,37 +29,39 @@
     (with-handlers ([exn:fail:resource?
                      (λ (e) (case (exn:fail:resource-resource e)
                               [(time) (dump-memory-stats) (flush-output)
-                               (printf "Timeout~%")]
-                              [(memory) (printf "Exhausted memory~%")]))])
-      (with-limits 3600 4096
-                   (call-with-values
-                       (λ () (begin0 (time ((aval) e))
-                                     (void)
-                                     (dump-memory-stats)
-                                     (flush-output)
-                                     (printf "Complete~%")))
-                     print-values)))))
+                               (printf "Result: Timeout~%")]
+                              [(memory) (printf "Result: Exhausted memory~%")]))]
+                    [exn:fail? (λ (e) (printf "Barf ~a ~%" e))])
+      (with-limits 3600 2048
+                   (begin0 (time ((aval) e))
+                           (void)
+                           (dump-memory-stats)
+                           (flush-output)
+                           (printf "Result: Complete~%"))))))
 
 (define test-file
   (command-line #:once-any
-                [("--baseline") "Benchmark baseline"
+                [("--bl") "Benchmark baseline"
                  (aval baseline)] ;; least optimized
-                [("--0cfa^") "Benchmark specialized fixpoint"
+                [("--sp") "Benchmark specialized fixpoint"
                  (aval 0cfa^)]
-                [("--lazy-0cfa^") "Benchmark specialized lazy non-determinism"
+                [("--ls") "Benchmark specialized lazy non-determinism"
                  (aval lazy-0cfa^)]
-                [("--lazy-0cfa^/c") "Benchmark compiled specialized lazy non-determinism"
+                [("--lc") "Benchmark compiled specialized lazy non-determinism"
                  (aval lazy-0cfa^/c)]
-                [("--lazy-0cfa∆/c")
+                [("--ld")
                   "Benchmark compiled store-diff lazy non-determinism"
                  (aval lazy-0cfa∆/c)]
-                [("--lazy-0cfa^/c!")
+                [("--li")
                  "Benchmark compiled imperative store lazy non-determinism"
                  (aval lazy-0cfa^/c!)]
-                [("--lazy-0cfa^/c/prealloc!")
+                [("--lp")
                  "Benchmark compiled preallocated store lazy non-determinism"
-                 (aval lazy-0cfa^/c!)] ;; most optimized
+                 (aval lazy-0cfa^/c/prealloc!)] ;; most optimized
                 ;; Not benchmarked for paper
+                [("--spp")
+                 "Benchmark compiled preallocated store sparse lazy non-determinism"
+                 (aval sparse-lazy-0cfa^/c/prealloc!)]
                 [("--lazy-0cfa")
                  "Benchmark specialized narrow lazy non-determinism"
                  (aval lazy-0cfa)]
@@ -82,4 +82,4 @@
                  (aval lazy-0cfa^/prealloc!)]
                 #:args (filename)
                 filename))
-(test test-file)
+(test (prep test-file))
