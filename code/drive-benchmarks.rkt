0b6fb27b0e653a09fb4fc405e5351970d93be429
J. Ian Johnson <ianj@ccs.neu.edu>
2012-11-06 15:50:12 -0500

    Driver updated to skip timeouts.

--- code/drive-benchmarks.rkt
+++ code/drive-benchmarks.rkt
@@ -7,34 +7,58 @@
   (define-values (base filename dir?) (split-path path))
   (define outtime (path-replace-suffix filename (format ".~a.time.~a" which run-num)))
   (define outmem (path-replace-suffix filename (format ".~a.mem.~a" which run-num)))
-  (format "racket run-benchmark.rkt --~a ~a > ~a 2> ~a" which file outtime outmem))
+  (format "racket run-benchmark.rkt --~a ~a > ~a 2> bench/~a" which file outtime outmem))
 
+(define church "../benchmarks/church.sch")
+(define mbrotZ "../benchmarks/mbrotZ.sch")
+(define earley "../benchmarks/earley.sch")
+(define boyer "../benchmarks/toplas98/boyer.sch")
+(define graphs "../benchmarks/toplas98/graphs.sch")
+(define lattice "../benchmarks/toplas98/lattice.scm")
+(define matrix "../benchmarks/toplas98/matrix.scm")
+(define maze "../benchmarks/toplas98/maze.sch") #;call/cc
+(define nbody "../benchmarks/toplas98/nbody.sch")
+(define nucleic "../benchmarks/toplas98/nucleic.sch")
 (define to-test
-  (list "../benchmarks/church.sch"
-        "../benchmarks/mbrotZ.sch"
-        "../benchmarks/earley.sch"
-        "../benchmarks/toplas98/boyer.sch"
-        "../benchmarks/toplas98/graphs.sch"
-        "../benchmarks/toplas98/lattice.scm"
-        "../benchmarks/toplas98/matrix.scm"
-        "../benchmarks/toplas98/maze.sch" ;; call/cc
-        "../benchmarks/toplas98/nbody.sch"
-        "../benchmarks/toplas98/nucleic.sch"
-        ;;"../benchmarks/toplas98/splay.scm" ;; old match
-        ;;"../benchmarks/toplas98/nucleic2.sch" ;; define-syntax
-        ;;"../benchmarks/toplas98/handle.scm" ;; old match and defmacro
-))
+  (list church mbrotZ earley boyer graphs lattice matrix maze nbody nucleic
+   ;;"../benchmarks/toplas98/splay.scm" ;; old match
+   ;;"../benchmarks/toplas98/nucleic2.sch" ;; define-syntax
+   ;;"../benchmarks/toplas98/handle.scm" ;; old match and defmacro
+   ))
+
+(define baseline "bl")
+(define specialized "sp")
+(define lazy "ls")
+(define compiled "lc")
+(define deltas "ld")
+(define imperative "li")
+(define preallocated "lp")
 
 (define which-analyses
-  (list  "bl"
-         "sp"
-         "ls"
-         "lc"
-         "ld"
-         "li"
-         "lp"))
+  (list baseline 
+        specialized
+        lazy
+        compiled
+        deltas
+        imperative
+        preallocated))
+
+(define known-timeout (hash baseline    (set boyer graphs matrix nbody)
+                            specialized (set boyer graphs matrix nbody)
+                            lazy        (set boyer graphs matrix nbody)
+                            compiled    (set boyer graphs matrix)
+                            deltas      (set church mbrotZ)))
+;; 2GB RAM
+(define known-exhaust (hash baseline (set nucleic)
+                            specialized (set nucleic)
+                            lazy (set nucleic)
+                            compiled (set nucleic)))
 
 (for* ([which which-analyses]
-       [file to-test])
+       [timeout (in-value (hash-ref known-timeout which (set)))]
+       [exhaust (in-value (hash-ref known-timeout which (set)))]
+       [file to-test]
+       #:unless (or (set-member? timeout file)
+                    (set-member? exhaust file)))
   (printf "Running ~a: ~a~%" which file)
   (system (construct-cmd which file)))
