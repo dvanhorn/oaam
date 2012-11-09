683013e8643c7840ffcaf352d191cfef74e234f1
David Van Horn <dvanhorn@ccs.neu.edu>
2012-11-08 22:36:43 -0500

    Initial plot of numbers.

--- paper/plot.rkt
+++ paper/plot.rkt
@@ -21,10 +21,11 @@
     ("li" . "imperative")
     ("lp" . "preallocated")))
 
-(define (c x) (+ x)) ;; neg for B/W, pos for color
+(define (c x) (- (sub1 x))) ;; neg for B/W, pos for color
+(define (next x) (sub1 x))
 
 (plot-width (* 2 (plot-width)))
-(plot-height (* 2 (plot-height)))
+(plot-height (* 3/2 (plot-height)))
 
 (define (p)
   (let ([which run])
@@ -39,15 +40,22 @@
             (discrete-histogram numbers*
                                 #:label name
                                 #:skip 10.5 #:x-min i
-                                #:color (c (add1 i)) #:line-color (c (add1 i))))
+                                #:style i
+                                #:color -7 #;(c (next i)) #:line-color "black" 
+                                #:line-style i))
           
-          #:y-label "Time relative to baseline (smaller is better)"
+          #:y-label "Time relative to baseline on log scale (smaller is better)"
           #:x-label "Optimization technique (cumulative, left to right)"
-          #:legend-anchor 'top-right)))
+          #:legend-anchor 'top-right
+          #:out-file "rel-time.ps")))
 
-(p)
 (parameterize ([plot-y-transform (axis-transform-bound log-transform 0.0001 1)]
-               #;[plot-y-ticks     (log-ticks)])
+               #;[plot-y-ticks     (log-ticks)]
+               [plot-font-size 20]
+               [line-color  "black"]
+               [interval-color  "black"]
+               [interval-line1-color  "black"]
+               [interval-line2-color  "black"])
   (p))
 
 
