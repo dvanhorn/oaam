834d0e8ed3c7f016c538b7448f854e89024d140e
J. Ian Johnson <ianj@ccs.neu.edu>
2012-11-14 00:41:09 -0500

    Fix for printing rate on resource limit exception.

--- code/handle-limits.rkt
+++ code/handle-limits.rkt
@@ -1,22 +1,26 @@
 #lang racket/base
 (require racket/sandbox)
-(provide with-limit-handler state-rate)
+(provide with-limit-handler state-count start-time state-rate)
 
-(define (state-rate start-time-in-ms state-count)
-  (define time-taken-in-seconds (/ (- (current-milliseconds) start-time-in-ms)
+(define state-count (make-parameter #f))
+(define start-time (make-parameter #f))
+
+(define (state-rate)
+  (define time-taken-in-seconds (/ (- (current-milliseconds)
+                                      (unbox (start-time)))
                                    1000))
   (printf "States/second: ~a~%" (exact->inexact ;; for decimal places
-                                 (/ (if (box? state-count)
-                                       (unbox state-count)
-                                       state-count)
+                                 (/ (unbox (state-count))
                                    time-taken-in-seconds))))
 
-(define-syntax-rule (with-limit-handler (start-time state-count) body ...)
-  (with-handlers ([exn:fail:resource?
-                   (λ (e)
-                      (state-rate start-time state-count)
-                      (case (exn:fail:resource-resource e)
-                        [(time) (dump-memory-stats) (printf "Result: Timeout~%")]
-                        [(memory) (printf "Result: Exhausted memory~%")]))]
-                  [exn:fail? (λ (e) (printf "Barf ~a" e))])
-    body ...))
+(define-syntax-rule (with-limit-handler body ...)
+  (parameterize ([state-count (box #f)]
+                 [start-time (box #f)])
+    (with-handlers ([exn:fail:resource?
+                     (λ (e)
+                        (state-rate)
+                        (case (exn:fail:resource-resource e)
+                          [(time) (dump-memory-stats) (printf "Result: Timeout~%")]
+                          [(memory) (printf "Result: Exhausted memory~%")]))]
+                    [exn:fail? (λ (e) (printf "Barf ~a" e))])
+      body ...)))
