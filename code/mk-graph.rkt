1694bbef11bad62475cfd92d0946c6bb99ac4001
David Van Horn <dvanhorn@ccs.neu.edu>
2012-11-28 00:31:04 -0500

    Trying to recreate dot files.
    mk-graph.rkt wasn't compiling due to name changes in
    kcfa-instantiations.rkt; added "^" to "0cfa" to at
    least reference something defined in order to compile.
    
    Cut stuff that was needed for the (commented out) system call to dot
    to render the .dot file to pdf.
    
    Changed path to be relative to the recently added
    benchmark/introspective.sch instead of an absolute path on ianj's file
    system.
    
    I tried to set generate-graph? to #t using syntax-parameterize, but I
    must be missing something because no dot files are created.
    
    So I'm still stuck trying to recreate those dot files.

--- code/mk-graph.rkt
+++ code/mk-graph.rkt
@@ -1,7 +1,8 @@
 #lang racket
 (require "kcfa-instantiations.rkt" "run-benchmark.rkt" "graph.rkt" (only-in "data.rkt" cons-limit))
+(require racket/stxparam)
 
-(define benchmark (make-parameter "/home/ianj/projects/xcfa/racket-impl/test-suite/introspective.sch"))
+(define benchmark (make-parameter "../benchmarks/introspective.sch"))
 (define (prefix)
   (let-values ([(base filename dir?) (split-path (string->path (benchmark)))])
     filename))
@@ -12,17 +13,15 @@
   (thread
    (λ ()
       (define p (prefix))
-      (define pdf (path-replace-suffix p (format "~a.pdf" suffix)))
-      (parameterize ([graph-file (path-replace-suffix p (format "~a.dot" suffix))]
-                     [aval eval])
-        (call-with-values (λ () (test (prep (benchmark)))) values)
-        #;
-        (system (format "dot -Tpdf -o ~a ~a" pdf (graph-file)))))))
+      (syntax-parameterize ([generate-graph? #t])
+        (parameterize ([graph-file (path-replace-suffix p (format "~a.dot" suffix))]
+                       [aval eval])
+          (call-with-values (λ () (test (prep (benchmark)))) values))))))
 
 (define (all-three)
-  (define ts (list (do 0cfa "-base")
-                   (do lazy-0cfa "-lazy")
-                   (do lazy-0cfa/c "-lazyc")))
+  (define ts (list (do 0cfa^ "-base")
+                   (do lazy-0cfa^ "-lazy")
+                   (do lazy-0cfa^/c "-lazyc")))
   (for ([t ts]) (thread-wait t)))
 
 (all-three)
