f9eda60913534ef85d5ee7eb3abef1170016f7d7
J. Ian Johnson <ianj@ccs.neu.edu>
2013-08-05 14:10:59 -0400

    Extended AST with contract monitoring forms.

--- code/ast.rkt
+++ code/ast.rkt
@@ -1,5 +1,5 @@
 #lang racket
-(require "notation.rkt")
+(require "notation.rkt" "tcon.rkt")
 (provide (all-defined-out))
 
 ;; An Exp is one of:
@@ -25,12 +25,25 @@
 (struct fal exp ()            #:transparent) ;; Fail
 (struct frm exp (r e)         #:transparent) ;; Frame
 (struct tst exp (r t e)       #:transparent) ;; Test
+;; Contract monitoring forms
+(struct mon exp (pℓ nℓ sℓ s e) #:transparent) ;; Structural contract
+(struct tmon exp (pℓ nℓ sℓ s t e) #:transparent) ;; Temporal contract attached to structural contract.
+;; Structural contract constructors
+(struct consc exp (ca cd)     #:transparent)
+(struct andc exp (c₀ c₁)      #:transparent)
+(struct orc exp (c₀ c₁)       #:transparent) ;; Only rightmost may be higher-order
+(struct fltc exp (e)          #:transparent)
+(struct -anyc exp ()          #:transparent) (define anyc (-anyc -1))
+(struct -nonec exp ()         #:transparent) (define nonec (-nonec -1))
+(struct arrc exp (name ncs pc) #:transparent)
+;; Note: Temporal contract constructors from tcon
+;; Top level timeline
+(struct -Λη ()) (define Λη (-Λη))
 
 (struct primr exp (which)    #:transparent)
 ;; (dst Lab Sym List[Pair[Sym Boolean]] Exp)
 ;; Define struct form that should die after we go to real Racket.
 (struct dst exp (name fields e) #:transparent)
-
 
 ;; Unmerged data.
 (struct datum exp (val) #:transparent)
@@ -41,18 +54,17 @@
 (struct mk-hash^ exp (keys vals) #:transparent)
 
 (define (free e)
-  (let loop* ([e e]
-              [bound (set)])
-    (define (loop e) (loop* e bound))
+  (define (expr-free e bound)
+    (define (loop e) (expr-free e bound))
     (match e
       [(var _ name) (if (name . ∈ . bound) ∅ (set name))]
       [(lrc _ xs es e)
        (define bound* (∪/l bound xs))
-       (for/union #:initial (loop* e bound*)
+       (for/union #:initial (expr-free e bound*)
                   ([e (in-list es)])
-         (loop* e bound*))]
-      [(lam _ vars body) (loop* body (∪/l bound vars))]
-      [(rlm _ vars rest body) (loop* body (∪1 (∪/l bound vars) rest))]
+         (expr-free e bound*))]
+      [(lam _ vars body) (expr-free body (∪/l bound vars))]
+      [(rlm _ vars rest body) (expr-free body (∪1 (∪/l bound vars) rest))]
       [(app _ rator rands) (for/union #:initial (loop rator)
                                       ([rand (in-list rands)])
                              (loop rand))]
@@ -60,7 +72,7 @@
       [(st! _ x e)
        (define efs (loop e))
        (if (x . ∈ . bound) efs (∪1 efs x))]
-      [(lcc _ x e) (loop* e (∪1 bound x))]
+      [(lcc _ x e) (expr-free e (∪1 bound x))]
       [(primr _ _) ∅]
       [(datum _ _) ∅]
       ;; Continuation mark forms
@@ -68,4 +80,20 @@
       [(frm _ _ e) (loop e)]
       [(fal _) ∅]
       [(tst _ _ t e) (∪ (loop t) (loop e))]
-      [_ (error 'free "Bad expr ~a" e)])))
+      ;; Monitoring forms
+      [(or (mon _ _ _ _ s e)
+           (tmon _ _ _ _ s _ e))
+       (∪ (scon-free s bound) (loop e))]
+      [_ (error 'free "Bad expr ~a" e)]))
+  (define (scon-free s bound)
+    (define (loop s) (scon-free s bound))
+    (match s
+      [(or (consc _ s₀ s₁)
+           (orc _ s₀ s₁)
+           (andc _ s₀ s₁))
+       (∪ (loop s₀) (loop s₁))]
+      [(fltc _ e) (expr-free e bound)]
+      [(arrc _ _ ncs pc)
+       (∪/l (loop pc) (map loop ncs))]
+      [(or (== anyc eq?) (== nonec eq?)) bound]))
+  (expr-free e ∅))
